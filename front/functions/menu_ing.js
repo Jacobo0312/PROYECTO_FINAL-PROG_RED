138575c86c10c8b5e370fb57da564deebc148adb
Estebanm1812 <EstebanMendoza02@hotmail.com>
2022-06-07 03:08:12 -0500

    more connection between buttons added

---
+++ front/functions/menu_ing.js
@@ -0,0 +1,19 @@
+document.getElementById('calculate').onclick = function(){calculate()};
+document.getElementById('addTechn').onclick = function(){addTechn()};
+document.getElementById('Dashboard').onclick = function(){Dashboard()};
+
+
+function calculate() {
+   
+    window.location.href = "calculateData.html";
+}
+function addTechn() {
+
+    window.location.href = "regTecnico.html";
+
+}
+function Dashboard() {
+    
+    window.location.href = "Dashboard.html";
+
+}
