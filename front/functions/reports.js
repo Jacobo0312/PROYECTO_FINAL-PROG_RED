c16e2c339dc215427c1e74d7f0b71f7e5860d0c5
Estebanm1812 <EstebanMendoza02@hotmail.com>
2022-06-07 07:45:09 -0500

    added most funcionts to reports

--- front/functions/reports.js
+++ front/functions/reports.js
@@ -16,13 +16,28 @@ function yesterdayDate() {
     var date = new Date();
     date.setDate(date.getDate() - 1);
     beginning.value = date;
-  
+   
 }
 function generateReport() {
-   
+    
+    
+    var datebegin = beginning.value;
+    var beginDate = new Date(datebegin);
+    var milisBegin = beginDate.getTime();
+    console.log(milisBegin);
+
+    var endDate = ending.value;
+    var dateEnd = new Date(endDate);
+    var milisEnd = dateEnd.getTime();
+    console.log(milisEnd);
+
+
 }   
 function todayDate() {
    
     var date = new Date();
     ending.value = date;
+    endingInMilis = date.getTime();
+    
+
 }
