32ec318f63c25e78c013979ae027a9c751ed1778
Estebanm1812 <EstebanMendoza02@hotmail.com>
2022-06-07 01:48:25 -0500

    New Windows added

---
+++ front/functions/crearSector.js
@@ -0,0 +1,39 @@
+document.getElementById('cancel').onclick = function(){returnPage()};
+document.getElementById('createSector').onclick = function(){createSector()};
+let sectorID = document.getElementById('sectorID');
+
+
+function returnPage() {
+   
+    window.location.href = "menu_admin.html";
+}
+function createSector() {
+   
+   if(sectorID.value == null){
+
+
+
+   }else{   
+
+    let sector ={
+
+        id: sectorID.value
+        
+    }
+    console.log("llego a la linea 23");
+    let json = JSON.stringify(sector);
+    console.log(json);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST','http://localhost:8080/sectors/add');
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(json);
+
+    window.alert('Sector Registrado');
+
+    window.location.href = "menu_admin.html";
+
+   }
+
+
+}
+
